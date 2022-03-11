fb06cae34e5d8dcd6ef435b2a639ee6d95aa9fe9
l-maridaa <79985945+l-maridaa@users.noreply.github.com>
2022-03-11 18:01:56 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -605,4 +605,3 @@ function displayShop(){
 let filter_col = document.querySelector('#filter-col')
 document.querySelector('#filter-toggle').addEventListener('click', () => filter_col.classList.toggle('active'));
 document.querySelector('#filter-close').addEventListener('click', () => filter_col.classList.toggle('active'));
-
